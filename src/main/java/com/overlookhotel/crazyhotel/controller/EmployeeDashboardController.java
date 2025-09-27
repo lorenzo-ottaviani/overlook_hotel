1b8e6629b7e335e79c85d1f1506d19981f569eee
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-09-27 18:32:31 +0200

    feat: upgarde front and add dashboard first structure
    Add a CSS file. Create a first dashoboard front strucuture. Add a switch page system from staff-login, customer-login and customer-register pages.

--- src/main/java/com/overlookhotel/crazyhotel/controller/EmployeeDashboardController.java
+++ src/main/java/com/overlookhotel/crazyhotel/controller/EmployeeDashboardController.java
@@ -1,7 +1,17 @@
 package com.overlookhotel.crazyhotel.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeDashboardController {
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeDashboardController.class);
+
+    @GetMapping("/employee/dashboard")
+    public String showEmployeeDashboardPage() {
+        logger.debug("Display of the employee dashboard page.");
+        return "employee/employee-dashboard";
+    }
 }
